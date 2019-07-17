185097581589184bac274d64926a44f35741d1a8
chenletneg <chenleteng@52tt.com>
2019-07-17 19:50:01 +0800

    iii

--- kafka-provider/src/main/java/com/clt/kafka/provider/controller/SendMsgController.java
+++ kafka-provider/src/main/java/com/clt/kafka/provider/controller/SendMsgController.java
@@ -1,15 +1,24 @@
 package com.clt.kafka.provider.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.clt.kafka.provider.service.KafkaSenderService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class SendMsgController {
 
+    @Resource
+    private KafkaSenderService kafkaSenderService;
+
     @RequestMapping(value = "/send",method = RequestMethod.GET)
     public String send(String msg){
-
+        JSONObject json = JSON.parseObject(msg);
+        kafkaSenderService.sendEvent(json);
         return "success";
     }
 
